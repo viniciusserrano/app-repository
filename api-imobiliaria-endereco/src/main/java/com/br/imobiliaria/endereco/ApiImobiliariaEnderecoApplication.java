c03ac4fb3da3d6c5ba95f07f0b5a1b663c462f99
Vinicius Gomes Serrano Segalio <vinicius.segalio@lello.com.br>
2025-04-19 00:54:56 -0300

    feature/01: Criando service de endereco

---
+++ api-imobiliaria-endereco/src/main/java/com/br/imobiliaria/endereco/ApiImobiliariaEnderecoApplication.java
@@ -0,0 +1,13 @@
+package com.br.imobiliaria.endereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiImobiliariaEnderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiImobiliariaEnderecoApplication.class, args);
+	}
+
+}
---
+++ api-imobiliaria-endereco/src/test/java/com/br/imobiliaria/endereco/ApiImobiliariaEnderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.imobiliaria.endereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiImobiliariaEnderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
